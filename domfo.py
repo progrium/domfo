bc30575657154d9d87a3bf61475087ebba0c3299
Brian Klug <brian.klug@gmail.com>
2011-03-03 08:44:42 +0800

    Two notable changes:
    1. The software first checks redirect.hostname.domainname.com for a TXT record, since technically, CNAME records may not have sibling resources.  Using the TXT record on the same hostname as a CNAME was causing problems on one known DNS provider (Amazon Route 53).
    
    2. The path is only appended if the location contains two slashes.  Previously the path was always being appended, such that:
    
    Given the location "http://foo.com/xyz", if the user went to "http://abc.com/123", the target would be "http://foo.com/xyz123".
    
    The location field should now be used as following:
    
    location=http://google.com -- if you want domfo to append the path automatically
    location=http://google.com/xyz -- if you want to hardcode a certain path
    location=http://google.com/ -- if you want to hardcode /

--- domfo.py
+++ domfo.py
@@ -9,7 +9,7 @@ from twisted.web.server import Site, NOT_DONE_YET
 from twisted.web.resource import Resource
 from twisted.python import log
 from twisted.names import client
-import sys, os
+import sys, os, string
 
 def opt(name, default=None, cast=str): return cast(os.environ.get(name, default))
 
@@ -21,20 +21,30 @@ class RedirectResource(Resource):
     
     def render_GET(self, request):
         host = request.requestHeaders.getRawHeaders('Host', [None])[0]
+        host = 'redirect.' + host.split(':')[0] # Drop the port
+        print "Looking up (new method): %s" % host
+        self.resolver.lookupText(host) \
+            .addCallback(lambda r: self.do_redirect(host, r[0], request)) \
+            .addErrback(lambda e: self.backwards_compat(request))
+        return NOT_DONE_YET
+
+    def backwards_compat(self, request):
+        host = request.requestHeaders.getRawHeaders('Host', [None])[0]
         host = host.split(':')[0] # Drop the port
-        print "Looking up: %s" % host
+        print "Looking up (old method): %s" % host
         self.resolver.lookupText(host) \
             .addCallback(lambda r: self.do_redirect(host, r[0], request)) \
             .addErrback(lambda e: self.do_error(host, e, request))
         return NOT_DONE_YET
-    
+
     def do_redirect(self, host, answers, request):
         for x in answers:
             data = str(x.payload.data[0])
             if data.startswith('location='):
                 location = data.split('=')[1]
                 location = location[0:-1] if location[-1] == '/' else location
-                location = location + request.path
+                if string.count(location,"/") == 2:
+                  location = location + request.path
                 request.redirect(location)
                 print "%s -> %s" % (host, location)
         request.finish()
